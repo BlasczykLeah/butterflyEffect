dab9f7fe6b7306ff10d8fc0206a1f00f331d634c
SebaldDM13 <64290663+SebaldDM13@users.noreply.github.com>
2020-10-19 20:04:54 -0500

    Text delay example with maxVisibleCharacters

---
+++ butterflyEffect_Project/Assets/Scripts/TextDelay2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextDelay2 : MonoBehaviour
+{
+    [SerializeField] private float typeSpeed = 0.08f;
+    private bool isTyping;
+    private TextMeshProUGUI myText;
+    private Coroutine typeCoroutine;
+
+    public void EnterText(string message)
+    {
+        if(message != null)
+        {
+            myText.text = message;
+            typeCoroutine = StartCoroutine(TypeCharacters(typeSpeed));
+        }
+    }
+
+    IEnumerator TypeCharacters(float time)
+    {
+        isTyping = true;
+        for(int i = 0; i <= myText.textInfo.characterCount; i++)
+        {
+            myText.maxVisibleCharacters = i;
+            yield return new WaitForSeconds(time);
+        }
+        isTyping = false;
+    }
+
+    void Start()
+    {
+        myText = GetComponent<TextMeshProUGUI>();
+        EnterText("some really long message that is super long and would take a long time to type");
+    }
+
+    void Update()
+    {
+        if(isTyping && Input.GetMouseButtonDown(0))
+        {
+            StopCoroutine(typeCoroutine);
+            isTyping = false;
+            myText.maxVisibleCharacters = myText.textInfo.characterCount;
+        }
+    }
+}
